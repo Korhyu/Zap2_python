c50edebab0721e97a93b48b9e0dd4484718f77ef
JoseT <jose_tantera@yahoo.com.ar>
2020-02-16 18:09:38 -0300

    asd

--- sql_functions.py
+++ sql_functions.py
@@ -1,76 +1,50 @@
 import mysql.connector
 from mysql.connector import Error
 
+datos = ''' host='localhost',
+            database='zap2',
+            user='zap2app',
+            password='zap2app' '''
 
 class funcionesSQL:
     def __init__ (self):
-        connection = mysql.connector.connect(   host='localhost',
-                                                database='zap2',
-                                                user='zap2app',
-                                                password='zap2app')
-
-        try: 
-            cursor = connection.cursor()
-
-        except mysql.connector.Error as error:
-            print("Error al insertar en la base de datos {}".format(error))
-
-        finally:
-            if (connection.is_connected()):
-                cursor.close()
-                print("Conexion cerada")
-
-    def test_conecction(self):
-        try:
-            connection = mysql.connector.connect(   host='localhost',
-                                                    database='zap2',
-                                                    user='zap2app',
-                                                    password='zap2app') 
-            cursor = connection.cursor()
-
-        except mysql.connector.Error as error:
-            print("Error al insertar en la base de datos {}".format(error))
-
-        finally:
-            if (connection.is_connected()):
-                print("Conexion exitosa")
-                cursor.close()
-                connection.close()
-                print("Conexion cerada")
+        pass
 
     def insertar_medicion_db(self, instalacion, tipo_med, tiempo, valor):
         try:
-            cursor = self.connection.cursor()
+            connection = mysql.connector.connect( datos )
+            cursor = connection.cursor()
             mySql_insert_query = """INSERT INTO medicion (instalacion, tipom, timestamp, valor) VALUES (%s, %s, %s, %s) """
             recordTuple = (instalacion, tipo_med, tiempo, valor)
             cursor.execute(mySql_insert_query, recordTuple)
-            self.connection.commit()
-            print("Datos insertados en la tabla medicion")
-
-        except mysql.connector.Error as error:
-            print("Error al insertar en la base de datos {}".format(error))
-
-        finally:
-            if (self.connection.is_connected()):
-                cursor.close()
-                self.connection.close()
-                print("Conexion cerada")
-
-
-    def insertar_prueba_db(self, cadena, valor):
-        try:
-            cursor = self.connection.cursor()
-            mySql_insert_query = """INSERT INTO prueba (cadena, valor) VALUES (%s, %s) """
-            recordTuple = (cadena, valor)
-            cursor.execute(mySql_insert_query, recordTuple)
-            self.connection.commit()
+            connection.commit()
             print("Datos insertados en la tabla prueba")
 
         except mysql.connector.Error as error:
             print("Error al insertar en la base de datos {}".format(error))
 
         finally:
-            if (self.connection.is_connected()):
+            if (connection.is_connected()):
                 cursor.close()
-                self.connection.close()
+                connection.close()
+                print("Conexion cerada")
+
+
+    def insertar_prueba_db(self, cadena, valor):
+        try:
+            connection = mysql.connector.connect( datos )
+            cursor = connection.cursor()
+            mySql_insert_query = """INSERT INTO prueba (cadena, valor) VALUES (%s, %s) """
+            recordTuple = (cadena, valor)
+            cursor.execute(mySql_insert_query, recordTuple)
+            connection.commit()
+            print("Datos insertados en la tabla prueba")
+
+        except mysql.connector.Error as error:
+            print("Error al insertar en la base de datos {}".format(error))
+
+        finally:
+            if (connection.is_connected()):
+                cursor.close()
+                connection.close()
                 print("Conexion cerada")
