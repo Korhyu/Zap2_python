853fdc854fb1e115b3d39954ab4738f5bca750b0
JoseT <jose_tantera@yahoo.com.ar>
2020-02-22 19:20:37 -0300

    asd

--- zap2_server.py
+++ zap2_server.py
@@ -1,15 +1,15 @@
 
-
+import paho.mqtt.client as mqtt
 from mqtt_functions import mqtt_obj
 from sql_functions import funcionesSQL
 
 
-conex_mqtt = mqtt_obj()
-conex_sql = funcionesSQL()
+obj_mqtt = mqtt_obj()
+obj_sql = funcionesSQL()
 
 
 # Me conecto a todos los Topics e indico las funciones que atienden a cada topic
-conex_mqtt.on_connect()
+conex_mqtt = mqtt_obj.connect_server("localhost", 1883, 60)
 
 # Pruebo la conexion a la db
 conex_sql.test_db_conn()
