54c91111af913b19ae5b08d969935f78d316c01a
JoseT <jose_tantera@yahoo.com.ar>
2020-02-24 12:12:35 -0300

    asd

--- prueba_servidor2.py
+++ prueba_servidor2.py
@@ -29,7 +29,7 @@ def esc_callback(client, userdata, message):
 
     try:
         cur = db.cursor()
-        instruccion = """INSERT INTO `prueba`(`cadena`, `coma`) VALUES ('test', '""" + message.payload.decode("utf-8") + """')"""
+        instruccion = """INSERT INTO `prueba`(`cadena`, `coma`) VALUES ('test', """ + message.payload.decode("utf-8") + """);"""
         datos = message.payload.decode("utf-8")
         print("QUERY: " + instruccion)
         cur.execute(instruccion)
