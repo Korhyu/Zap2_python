bcfc03661d642bc06a232b977c933304b91105e9
JoseT <jose_tantera@yahoo.com.ar>
2020-02-25 13:14:12 -0300

    asd

--- zap2_server.py
+++ zap2_server.py
@@ -42,8 +42,9 @@ def on_message_test(client, userdata, msg):
 
 def on_message_tension(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
-    print(msg.topic + " " + str(int(msg.payload, 2)))
-    print(msg.topic + " " + str(float(int(msg.payload, 2))))
+    print(msg.topic + " " + str(msg.payload.decode("utf-8")))
+    print(msg.topic + " " + str(int(msg.payload.decode("utf-8"))))
+    print(msg.topic + " " + str(float(int(msg.payload.decode("utf-8"))/1000)))
     try:
         valor = float(int(msg.payload.decode("utf-8"))/1000)
         datos.v.append(valor)
