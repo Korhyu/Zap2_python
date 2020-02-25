4219e5a63a9e89accd34ad7110372ad11828c273
JoseT <jose_tantera@yahoo.com.ar>
2020-02-25 20:45:13 -0300

    asd

--- zap2_server.py
+++ zap2_server.py
@@ -29,7 +29,7 @@ def on_message_f_sampl(client, userdata, msg):
 def on_message_t_muest(client, userdata, msg):
     global datos
     valor = float(msg.payload.decode("utf-8"))
-    datos.tm(valor)
+    datos.tm = valor
     print("Tiempo de muestreo modificado: " + str(datos.tm))
 
 
