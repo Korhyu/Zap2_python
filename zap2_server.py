963f8c6b1806592438c97a1e527fccc3216b2d61
JoseT <jose_tantera@yahoo.com.ar>
2020-02-25 12:53:38 -0300

    asd

--- zap2_server.py
+++ zap2_server.py
@@ -13,8 +13,7 @@ from codecs import decode
 
 def bin_to_float(b):
     """ Convert binary string to a float. """
-    bf = int_to_bytes(int(b, 2), 8)  # 8 bytes needed for IEEE 754 binary64.
-    return struct.unpack('>d', bf)[0]
+    return bin(struct.unpack('!I', struct.pack('!f', num))[0])[2:].zfill(32)
 
 
 def on_message_f_sampl(self, client, userdata, msg):
