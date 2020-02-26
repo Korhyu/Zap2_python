9aa5d9e7b65f2bacdc193924c8ee1b13caec67cd
JoseT <jose_tantera@yahoo.com.ar>
2020-02-26 20:19:48 -0300

    asd

--- sql_functions.py
+++ sql_functions.py
@@ -3,19 +3,20 @@ from mysql.connector import Error
 import datetime
 from electric_data import electric_data
 
+"""
 datos = ''' host='localhost',
             database='zap2',
             user='zap2app',
             password='zap2app' '''
-
+"""
 
 class funcionesSQL:
     def __init__ (self):
 
-        datos = ''' host='localhost',
-                    database='zap2',
-                    user='zap2app',
-                    password='zap2app' '''
+        self.conn = ''' host='localhost',
+                        database='zap2',
+                        user='zap2app',
+                        password='zap2app' '''
 
         pass
 
@@ -40,10 +41,7 @@ class funcionesSQL:
 
     def insertar_prueba_db(self, cadena, valor):
         try:
-            connection = mysql.connector.connect(   host='localhost',
-                                                    database='zap2',
-                                                    user='zap2app',
-                                                    password='zap2app' )
+            connection = mysql.connector.connect( self.conn )
             cursor = connection.cursor()
             mySql_insert_query = """INSERT INTO prueba (cadena, coma) VALUES (%s, %s) """
             recordTuple = (cadena, valor)
@@ -62,7 +60,7 @@ class funcionesSQL:
 
     def insertar_medicion_db(self, instalacion, tipo_med, tiempo, valor):
         try:
-            connection = mysql.connector.connect( self.datos )
+            connection = mysql.connector.connect( self.conn )
             cursor = connection.cursor()
             mySql_insert_query = """INSERT INTO medicion (instalacion, tipom, time_id, valor) VALUES (%s, %s, %s, %s) """
             recordTuple = (instalacion, tipo_med, tiempo, valor)
@@ -81,7 +79,7 @@ class funcionesSQL:
 
     def insertar_timestamp_db(self, timestamp, uso_horario = None):
         try:
-            connection = mysql.connector.connect( self.datos )
+            connection = mysql.connector.connect( self.conn )
             cursor = connection.cursor()
             mySql_insert_query = """INSERT INTO tiempo (timestamp, uso_horario) VALUES (%s, %s) """
             recordTuple = (timestamp, uso_horario)
