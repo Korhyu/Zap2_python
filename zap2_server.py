5f93722f493e494df75f0dd56a035a221aa221e6
JoseT <jose_tantera@yahoo.com.ar>
2020-02-26 21:09:48 -0300

    asd

--- zap2_server.py
+++ zap2_server.py
@@ -54,10 +54,10 @@ def on_connect(client, userdata, flags, rc):
         print("\t" + str(MQTT_TOPICS[j][0]))
 
 
-def on_message_test(self, client, userdata, msg):
+def on_message_test(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
-def on_message_time(self, client, userdata, msg):
+def on_message_time(client, userdata, msg):
     global datos
     
     #tag = str(msg.payload.decode("utf-8"))
